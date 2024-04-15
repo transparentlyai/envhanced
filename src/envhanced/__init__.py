ca5f9b0f6a22834496361a5b5cbf57dcf4187ebd
mauroai <mauro@transparently.ai>
2024-04-15 14:11:10 +0800

    added support for .secret.env file

--- src/envhanced/__init__.py
+++ src/envhanced/__init__.py
@@ -32,6 +32,7 @@ class Config:
         self.defaults_path = defaults or f"{cwd}/defaults.env"
         self.environ_path = environ or f"{cwd}/environ.env"
         self.secrets_path = secrets or f"{cwd}/secrets.env"
+        self.secrets_path = secrets or f"{cwd}.secrets.env"
         self.reload()
 
     def reload(self):
